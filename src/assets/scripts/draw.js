8ed65a71e093cdd5235e90c5461cbc932428ab4d
mosquito chang <bbbb00y@gmail.com>
2017-11-16 17:26:56 +0800

    findcolor draw ui update

--- src/assets/scripts/draw.js
+++ src/assets/scripts/draw.js
@@ -22,31 +22,45 @@ function canvasResize() {
 
 
 //fake colorlist
-var colorlist = [];
-var rgbChange = 10;
-for (var r = 0; r < 256; r = r + parseInt(255/rgbChange)) {
-	for (var g = 0; g < 256; g = g + parseInt(255/rgbChange)) {
-		for (var b = 0; b < 256; b = b + parseInt(255/rgbChange)) {
-			var rHex = r.toString(16);
-			if(rHex.length<2) {
-				rHex = "0" + rHex;
-			}
-			var gHex = g.toString(16);
-			if(gHex.length<2) {
-				gHex = "0" + gHex;
-			}
-			var bHex = b.toString(16);
-			if(bHex.length<2) {
-				bHex = "0" + bHex;
-			}
-			var hex = "#" + rHex + gHex + bHex;
-			var name = hex;
-			colorlist.push({hex,name,r,g,b});
-		}
-	}
-}
-console.log(colorlist);
+// var colorlist = [];
+// var rgbChange = 10;
+// for (var r = 0; r < 256; r = r + parseInt(255/rgbChange)) {
+// 	for (var g = 0; g < 256; g = g + parseInt(255/rgbChange)) {
+// 		for (var b = 0; b < 256; b = b + parseInt(255/rgbChange)) {
+// 			var rHex = r.toString(16);
+// 			if(rHex.length<2) {
+// 				rHex = "0" + rHex;
+// 			}
+// 			var gHex = g.toString(16);
+// 			if(gHex.length<2) {
+// 				gHex = "0" + gHex;
+// 			}
+// 			var bHex = b.toString(16);
+// 			if(bHex.length<2) {
+// 				bHex = "0" + bHex;
+// 			}
+// 			var hex = "#" + rHex + gHex + bHex;
+// 			var name = hex;
+// 			colorlist.push({hex,name,r,g,b});
+// 		}
+// 	}
+// }
+// console.log(colorlist);
 
+var colorlist = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "../assets/scripts/json/BenjaminMoore_ClassicColors_en-us.json",
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})(); 
+console.log(colorlist);
 
 //選色時被觸發
 //進行搜尋並呈現結果
@@ -55,7 +69,9 @@ function colorPickerChange(color) {
 	var searchResult = getColorRange(color);
 	var colorDomTemplate = "<div data-hex='{{hex}}' data-name='{{name}}' class='color-select__color'></div>"
 	for (var i = 0; i < searchResult.length; i++) {
-		var colorDom = colorDomTemplate.replace("{{hex}}",searchResult[i].color.hex)
+		var rgb = [parseInt(searchResult[i].color.color[0]*255),parseInt(searchResult[i].color.color[1]*255),parseInt(searchResult[i].color.color[2]*255)]
+		var hex = rgbToHex(rgb[0],rgb[1],rgb[2]);
+		var colorDom = colorDomTemplate.replace("{{hex}}",hex)
 										.replace("{{name}}",searchResult[i].color.name);
 		$(".color-select__list").append(colorDom);
 	}
@@ -63,7 +79,10 @@ function colorPickerChange(color) {
 	     $(this).css("background-color",$(this).data('hex'));
 	});
 	$('.color-select__color').on("click", function(){
-		$(".color-info__color").css("background-color",$(this).data('hex'));
+		$(".color-info__color").css("background-color",$(this).data('hex') );
+		$(".color-info__name").html($(this).data('name'));
+		$(".color-select__color--selected").removeClass('color-select__color--selected');
+		$(this).addClass('color-select__color--selected');
 	})
 
 }
@@ -74,10 +93,10 @@ var maxDelta = 30; //最大差異值
 function getColorRange(color) {
 	var searchResult = [];
 	var center = hexToRgb(color);
-	for (var i = 0; i < colorlist.length; i++) {
-		var delta = Math.pow(Math.pow(colorlist[i].r-center.r,2)+Math.pow(colorlist[i].g-center.g,2)+Math.pow(colorlist[i].b-center.b,2),0.5);
+	for (var i = 0; i < colorlist.colors.length; i++) {
+		var delta = Math.pow(Math.pow(colorlist.colors[i].color[0]*255-center.r,2)+Math.pow(colorlist.colors[i].color[1]*255-center.g,2)+Math.pow(colorlist.colors[i].color[2]*255-center.b,2),0.5);
 		if(delta <= maxDelta) {
-			searchResult.push({color:colorlist[i],delta});
+			searchResult.push({color:colorlist.colors[i],delta});
 		}
 	}
 	//照差異度做排序
@@ -96,3 +115,27 @@ function hexToRgb(hex) {
         b: parseInt(result[3], 16)
     } : null;
 }
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+$(".color-info__like-btn").on("click",function(){
+	$(".color-info__like-btn").toggleClass('color-info__like-btn--liked');
+})
+
+$(".collection__opener").on("click",function(){
+	$(".fcLayout").toggleClass('fcLayout--collection-active');
+})
+
+$(".collection__edit-btn").on("click",function(){
+	$(".collection__edit-btn").toggleClass('collection__edit-btn--selected');
+	$('.color-item__delete-btn').each(function(i){
+	     $(this).toggleClass('color-item__delete-btn--active');
+	});
+})
